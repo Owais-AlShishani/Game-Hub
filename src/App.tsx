6d826a615ea1487a88379cb1753385d6c7440d8a
Owais-AlShishani <98020718+Owais-AlShishani@users.noreply.github.com>
2024-09-10 16:50:51 +0300

    Build responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
 
 function App() {
   return (
-    <Button colorScheme='blue'>Button</Button>
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }}>
+      <GridItem area={'nav'} bg={'coral'}>Nav</GridItem>
+      {/* only displayed on large screen aside */}
+      <Show above='lg'>
+        <GridItem area={'aside'} bg={'gold'}>Aside</GridItem>
+      </Show>
+      <GridItem area={'main'} bg={'dodgerblue'}>Main</GridItem>
+    </Grid>
   );
 }
 
