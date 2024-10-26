0b0ed268af71d43a14aeb481073ebb1f60b0bfa1
Owais-AlShishani <98020718+Owais-AlShishani@users.noreply.github.com>
2024-10-26 16:16:55 +0300

    Showing Spinner while fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,7 +1,11 @@
+import { Spinner } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
 
 const GenreList = () => {
-    const { data } = useGenres();
+    const { data, isLoading } = useGenres();
+
+    if (isLoading) return <Spinner />;
+
     return (
         <ul>
             {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
