2117383133beede99f2d46312d083d7f34e68037
Owais-AlShishani <98020718+Owais-AlShishani@users.noreply.github.com>
2024-11-28 19:49:11 +0300

    Ship genres with app

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -7,8 +7,9 @@ interface Props {
     selectedGenre: Genre | null;
 }
 const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
-    const { data, isLoading } = useGenres();
+    const { data, isLoading, error } = useGenres();
 
+    if (error) return null;
     if (isLoading) return <Spinner />;
 
     return (
